5a61b57682a0d97a62b791c92c4b6d1f32d3bdfd
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2025-06-13 09:14:31 +0200

    feat(): gh60 default 60ansi layout

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/rules.mk
+++
@@ -1,5 +0,0 @@
-MOUSEKEY_ENABLE  = no        # Mouse keys
-EXTRAKEY_ENABLE  = no        # Audio control and System control
-CONSOLE_ENABLE   = no        # Console for debug
-TAP_DANCE_ENABLE = no       # Tap dance a single key can do different things
-RGB_MATRIX_ENABLE = yes
